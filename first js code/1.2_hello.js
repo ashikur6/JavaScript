a2057f9cccd1068bd3e1b36ac889a28fce47212f
Md. Ashikur Rahman <140335715+ashikur6@users.noreply.github.com>
2025-11-16 15:22:24 +0600

    Update 1.2_hello.js

--- first js code/1.2_hello.js
+++ first js code/1.2_hello.js
@@ -3,3 +3,16 @@ console.log('hello world');
 consol.log('hello Ashik'):
 consol.log('hello Ashik2')
 consol.log('hello munna');
+
+// first_name 
+// firstName
+// FirstName 
+// FIRSTNAME 
+ // a-Z , 0-9 , _ , $ 
+
+let _name99 = 'Ashik';  // valid
+let $name99 = 'Rakib';  // valid 
+let 99name = 'Munna';   // Not Valid 
+
+
+Conxt TITLE = 'I Love You'; 
